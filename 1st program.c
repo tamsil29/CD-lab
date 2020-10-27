73e2731a2be1f7db3d033c6e6d4ce4c94124597f
tamsil29 <69435590+tamsil29@users.noreply.github.com>
2020-10-27 14:43:59 +0530

    Add files via upload

---
+++ 1st program.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+#include<stdlib.h>
+char a[20];
+int i,n,flag=0;
+void main()
+{
+clrscr();
+
+printf("enter string");
+gets(a);
+n=strlen(a);
+i=0;
+if(a[n-1]=='b'&&a[n-2]=='b'&&a[n-3]=='a')
+{
+while(i<n-3)
+{
+if(a[i]=='a'||a[i]=='b')
+{i++;
+flag=flag+1;
+}
+else
+{
+printf("not accepted");
+break;
+}
+      }
+}
+if(flag==n-3)
+{
+printf("string accepted");
+}
+getch();
+}
